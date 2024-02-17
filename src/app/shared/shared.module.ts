c7a55c0f302b5d75c785ab23ec89a1f1bf8694d8
Jhony Yañez <jhony.yaesz27@gmail.com>
2024-02-17 10:43:31 -0600

    navegacion atravez de rutas

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { HomePageComponent } from './pages/home-page/homePage.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { SidebarComponent } from './sidebar/sidebat.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
 
 
 
@@ -9,13 +14,18 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
   declarations: [
     AboutPageComponent,
    HomePageComponent,
+   SidebarComponent,
+   ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     AboutPageComponent,
-    HomePageComponent
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
